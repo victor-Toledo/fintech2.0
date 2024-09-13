f0e5f63d5d25bc5f87aa88f61b2a1853b2be1150
victor-Toledo <victor.ferrareto@escola.pr.gov.br>
2024-09-13 08:18:50 -0300

    vendas e produtos
    eu coloquei a tabela de vendas e a de produtos

--- models/cliente.js
+++ models/cliente.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const venda = require('./venda');
 module.exports = (sequelize, DataTypes) => {
   class Cliente extends Model {
     /**
@@ -10,7 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Cliente.hasMany(Venda, {
+        foreignKey: 'ClienteId' 
+      });
+      Venda.belongsTo(Cliente);
     }
   }
   Cliente.init({
